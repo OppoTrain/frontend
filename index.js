1eb22fc97c5b2d0b9b9a5c84f9f889fc3057d137
Abd-Alsalam-Jodalah <129756251+Abd-Alsalam-Jodalah@users.noreply.github.com>
2023-08-08 15:03:11 +0300

    -frontend #1 Landing page -modified structure

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener('scroll', () => {
+    const verticalScrollPx = window.scrollY || window.pageYOffset;
+  
+    if (verticalScrollPx > 130) {
+        console.log('hi');
+      //document.getElementById('containerone').style.backgroundColor='black';
+    } else if(verticalScrollPx <130) {
+        document.getElementById('containerone').style.backgroundColor = '#E5E7EB';
+// 
+        // document.getElementById('containerone').style.backgroundColor='linear-gradient(to bottom, #E5E7EB, #FFFFFF)'; 
+    
+    
+    } 
+  });
