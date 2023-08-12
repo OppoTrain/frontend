a13745c2b207ba9ec0fc420c5e0b124e4060605d
Tasnim <s12028850@stu.najah.edu>
2023-08-12 18:12:40 +0300

    add alt attribute for all img

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 window.addEventListener('scroll', () => {
     const verticalScrollPx = window.scrollY || window.pageYOffset;
-  
+   
     if (verticalScrollPx > 130) {
         console.log('hi');
         // document.getElementById('nav-bar').style.border=''
       //document.getElementById('containerone').style.backgroundColor='black';
     } else if(verticalScrollPx <130) {
         document.getElementById('containerone').style.backgroundColor = '#E5E7EB';
-// 
+//
         // document.getElementById('containerone').style.backgroundColor='linear-gradient(to bottom, #E5E7EB, #FFFFFF)'; 
-    
-    
     } 
   });
