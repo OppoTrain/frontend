9a774da9b87289cc7a2550d785dfdd986d6d8154
Tasnim <s12028850@stu.najah.edu>
2023-08-22 15:02:29 +0300

    Set moving lines below images and title

--- build/index.js
+++ build/index.js
@@ -18,9 +18,36 @@ document.addEventListener("DOMContentLoaded", () => {
         currentCarousel = currentCarousel === carousel1 ? carousel2 : carousel1;
         currentCarousel.style.display = "block"; 
     });
-});
+
 
 const spans = document.querySelectorAll(".color-changing-text span");
 spans.forEach((span, index) => {
     span.style.animationDelay = `${index * 0.1}s`;
+});
+
+});
+
+new Swiper(".wrapper", {
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    spaceBetween: 24,
+    lazyLoading: true,
+    loop: true,
+    keyboard: {
+       enabled: true
+    },
+    navigation: {
+      nextEl: "#right",
+      prevEl: "#left"
+    },
+    pagination: {
+    el: (".wrapper .swiper-custom-pagination"),
+    clickable: true,
+    renderBullet: function(index, className) {
+    return `<div class=${className}>
+    <span class="number">${index + 1}</span>
+    <span class="line"></span>
+    </div>`
+    }
+    }
 });
